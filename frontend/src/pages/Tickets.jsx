bc976290fda6a545e72fe74b16b40343c3d77790
LucaDvc <lukadvc1@gmail.com>
2023-11-14 16:16:24 +0200

    tickets page

--- frontend/src/pages/Tickets.jsx
+++ frontend/src/pages/Tickets.jsx
@@ -3,6 +3,8 @@ import { useSelector, useDispatch } from 'react-redux';
 import { getTickets, reset } from '../features/tickets/ticketSlice';
 import { toast } from 'react-toastify';
 import Spinner from '../components/Spinner';
+import BackButton from '../components/BackButton';
+import TicketItem from '../components/TicketItem';
 
 function Tickets() {
   const { isError, isLoading, isSuccess, message, tickets } = useSelector(
@@ -28,7 +30,23 @@ function Tickets() {
     return <Spinner />;
   }
 
-  return <div>Tickets</div>;
+  return (
+    <>
+      <BackButton url='/' />
+      <h1>Tickets</h1>
+      <div className='tickets'>
+        <div className='ticket-headings'>
+          <div>Date</div>
+          <div>Product</div>
+          <div>Status</div>
+          <div></div>
+        </div>
+        {tickets.map((ticket) => (
+          <TicketItem key={ticket._id} ticket={ticket} />
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default Tickets;
